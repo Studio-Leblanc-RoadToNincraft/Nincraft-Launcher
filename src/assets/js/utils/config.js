f60929c1ca3002db4248629f6b1e720733697e19
andre <61792917+luuxis@users.noreply.github.com>
2023-10-28 23:32:34 +0200

    add news api

--- src/assets/js/utils/config.js
+++ src/assets/js/utils/config.js
@@ -5,6 +5,7 @@
 
 const pkg = require('../package.json');
 const nodeFetch = require("node-fetch")
+const convert = require('xml-js');
 let url = pkg.user ? `${pkg.url}/${pkg.user}` : pkg.url
 
 let config = `${url}/launcher/config-launcher/config.json`;
@@ -39,7 +40,22 @@ class Config {
     async getNews() {
         return new Promise((resolve, reject) => {
             nodeFetch(news).then(async config => {
-                if (config.status === 200) return resolve(config.json());
+                if (config.status === 200) {
+                    let news = [];
+                    let response = await config.text()
+                    response = (JSON.parse(convert.xml2json(response, { compact: true })))?.rss?.channel?.item;
+
+                    if (typeof response == 'object') response = [response];
+                    for (let item of response) {
+                        news.push({
+                            title: item.title._text,
+                            content: item['content:encoded']._text,
+                            author: item['dc:creator']._text,
+                            publish_date: item.pubDate._text
+                        })
+                    }
+                    return resolve(news);
+                }
                 else return reject({ error: { code: config.statusText, message: 'server not accessible' } });
             }).catch(error => {
                 return reject({ error });
